370409cb48307d790fb8413046715941817516da
AdnanAdu <adnanpe5@gmail.com>
2025-03-01 16:34:32 +0530

    added supabse initial integration with apikey

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/core/secret/secret_key.dart';
+import 'package:notes_app/feature/notePage/view/pages/note_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Supabase.initialize(
+    url: "https://hqrvqdeesdbnfhqmtvrv.supabase.co",
+    anonKey: Secret.apiKey,
+  );
+
   runApp(const MyApp());
 }
 
@@ -12,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
+      home: const NotePage(),
     );
   }
 }
